c064b27a508a1397291c771a47b0800a5404ead5
Pau Sansa <pausansa59@gmail.com>
2023-01-18 12:57:48 +0100

    Iniciar n3exercici1

--- Tasca S1-02/src/n2exercici1/Main.java
+++ Tasca S1-02/src/n2exercici1/Main.java
@@ -6,8 +6,8 @@ public class Main {
 //        System.out.println(Entrada.llegirInt(("Introdueix un Int")));
 //        System.out.println(Entrada.llegirFloat(("Introdueix un Float")));
 //        System.out.println(Entrada.llegirDouble(("Introdueix un Double")));
-        System.out.println(Entrada.llegirChar(("Introdueix un Char")));
-//        System.out.println(Entrada.llegirString(("Introdueix un String ")));
+//        System.out.println(Entrada.llegirChar(("Introdueix un Char")));
+        System.out.println(Entrada.llegirString(("Introdueix un String ")));
 //        System.out.println(Entrada.llegirString(("Introdueix s o n")));
 
 
---
+++ Tasca S1-02/src/n3exercici1/Main.java
@@ -0,0 +1,12 @@
+package n3exercici1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void init() {
+
+    }
+}
