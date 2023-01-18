6dce1d72cc3386e4bc9e88c31eee49bc5c2818f4
Pau Sansa <pausansa59@gmail.com>
2023-01-18 12:33:28 +0100

    Acabar n2exercici1

---
+++ Tasca S1-02/src/n2exercici1/Main.java
@@ -0,0 +1,16 @@
+package n2exercici1;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println(Entrada.llegirChar("Introdueix un Char"));
+//        System.out.println(Entrada.llegirInt(("Introdueix un Int")));
+//        System.out.println(Entrada.llegirFloat(("Introdueix un Float")));
+//        System.out.println(Entrada.llegirDouble(("Introdueix un Double")));
+        System.out.println(Entrada.llegirChar(("Introdueix un Char")));
+//        System.out.println(Entrada.llegirString(("Introdueix un String ")));
+//        System.out.println(Entrada.llegirString(("Introdueix s o n")));
+
+
+
+    }
+}
