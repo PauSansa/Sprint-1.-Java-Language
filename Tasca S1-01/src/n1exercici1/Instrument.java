33022fb734555b6e30d12a36356e2353cde6d523
Sansa <pausansa59@gmail.com>
2023-01-18 15:23:57 +0100

    Privatitzar Atributs n1exercici1 y posar getters/setters

--- Tasca S1-01/src/n1exercici1/Instrument.java
+++ Tasca S1-01/src/n1exercici1/Instrument.java
@@ -1,8 +1,8 @@
 package n1exercici1;
 
 public abstract class Instrument {
-    String name;
-    double price;
+    private String name;
+    private double price;
 
     public Instrument(String name, double price) {
         this.name = name;
@@ -10,4 +10,20 @@ public abstract class Instrument {
     }
 
     public abstract void tocar();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
