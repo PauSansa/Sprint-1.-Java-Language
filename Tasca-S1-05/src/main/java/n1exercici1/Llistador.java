45d43fb11b7f8150ba063923138673174c7fc93b
Pau Sansa <pausansa59@gmail.com>
2023-01-24 13:03:10 +0100

    Progreso Barna

--- Tasca-S1-05/src/main/java/n1exercici1/Llistador.java
+++ Tasca-S1-05/src/main/java/n1exercici1/Llistador.java
@@ -1,15 +1,65 @@
 package n1exercici1;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class Llistador {
+
+    private int iteracions;
 
     public Llistador(){};
 
     public void llistarDiretori(File directori) {
-        System.out.printf("\n Directori Introduit: %s \n", directori.getAbsolutePath());
-        for (File subdirectoris: directori.listFiles()) {
-            System.out.println(subdirectoris.getName());
+        List<String> listSubDirectoris = new ArrayList<>();
+
+        if (directori.listFiles() != null){
+            for (File subdirectori: directori.listFiles()) {
+                if (subdirectori.length() != 0){
+                    listSubDirectoris.add(subdirectori.getName());
+                }
+
+            }
+            listSubDirectoris.sort(String::compareTo);
+
+            for (String nomDirectori: listSubDirectoris) {
+                System.out.println("    " + nomDirectori);
+            }
         }
+
+    }
+
+    public void llistarFile(File directori, int iteracions) {
+        String espai = "";
+        for (int i = 0; i < iteracions; i++) {
+            espai = espai.concat("  ");
+        }
+        System.out.println(espai + directori.getName());
+    }
+
+    public void llistarDirectory(File directori, int iteracions){
+
+    }
+
+    public void llistarDiretoriArbre(File directori) {
+
+
+        for (File fd : directori.listFiles()) {
+            //Si es un directori torna a executar aquest metode sumantli una iteracio
+            if(fd.isDirectory()){
+                llistarFile(fd,iteracions);
+                this.iteracions = iteracions + 1;
+                llistarDiretoriArbre(fd);
+            } else {
+                llistarFile(fd, this.iteracions);
+            }
+
+        }
+
+        iteracions = 0;
+
+
     }
 }
--- Tasca-S1-05/src/main/java/n1exercici1/n1exercici1M.java
+++ Tasca-S1-05/src/main/java/n1exercici1/n1exercici1M.java
@@ -2,11 +2,11 @@ package n1exercici1;
 
 import java.io.File;
 
-public class n1exercici1 {
+public class n1exercici1M {
 
     public static void main(String[] args) {
         Llistador ll = new Llistador();
         File directori = new File(".");
-        ll.llistarDiretori(directori);
+        ll.llistarDiretoriArbre(directori);
     }
 }
