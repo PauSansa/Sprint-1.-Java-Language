4f4d0e41bd307038208c4dcd1bdf07603deb6be0
Pau Sansa <pausansa59@gmail.com>
2023-01-25 12:39:26 +0100

    Borrar Todo

--- Tasca-S1-05/src/main/java/n1exercici1/Llistador.java
+++ Tasca-S1-05/src/main/java/n1exercici1/Llistador.java
@@ -1,42 +1,12 @@
 package n1exercici1;
 
 import java.io.File;
-import java.util.ArrayList;
-
-import java.util.List;
 
 public class Llistador {
+    private File file;
+    private String path;
 
-    private int iteracions;
+    public Llistador() {
 
-    public Llistador(){}
-
-    public void llistarDiretori(File directori) {
-        List<String> listSubDirectoris = new ArrayList<>();
-
-        if (directori.listFiles() != null){
-            for (File subdirectori: directori.listFiles()) {
-                if (subdirectori.length() != 0){
-                    listSubDirectoris.add(subdirectori.getName());
-                }
-
-            }
-            listSubDirectoris.sort(String::compareTo);
-
-            for (String nomDirectori: listSubDirectoris) {
-                System.out.println("    " + nomDirectori);
-            }
-        }
-
-    }
-
-    public void llistarDirectoriArbre(File directori) {
-        for (File df : directori.listFiles()) {
-            if (df.isDirectory()){
-                this.iteracions = iteracions + 1;
-                llistarDirectoriArbre(df);
-            } else if (df.isFile()) //Aqui
-
-        }
     }
 }
--- Tasca-S1-05/src/main/java/n1exercici1/n1exercici1M.java
+++ Tasca-S1-05/src/main/java/n1exercici1/n1exercici1M.java
@@ -5,9 +5,6 @@ import java.io.File;
 public class n1exercici1M {
 
     public static void main(String[] args) {
-        Llistador ll = new Llistador();
-        File directori = new File(".");
-        System.out.println(directori.getAbsolutePath());
-        ll.llistarDiretoriArbre(directori);
+
     }
 }
