da14ea95e884a4fe31bec7d347b75551a3d46290
Sansa <pausansa59@gmail.com>
2023-01-28 10:30:10 +0100

    Acabar n1exercici2

---
+++ Tasca-S1-06/src/n1exercici2/GenericMethods.java
@@ -0,0 +1,9 @@
+package n1exercici2;
+
+public class GenericMethods {
+    public static <T,U,V> void generic(T t1, U t2, V t3) {
+        System.out.println(t1);
+        System.out.println(t2);
+        System.out.println(t3);
+    }
+}
---
+++ Tasca-S1-06/src/n1exercici2/n1exercici2M.java
@@ -0,0 +1,8 @@
+package n1exercici2;
+
+public class n1exercici2M {
+    public static void main(String[] args) {
+        Persona p1 = new Persona("Pau", "Sansa", 19);
+        GenericMethods.generic(p1, 997, "String");
+    }
+}
